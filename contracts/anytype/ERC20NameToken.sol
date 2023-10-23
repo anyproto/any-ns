382eeaa8e1ff63c61e015956c66612bbcf887cb6
Anton Akentev <anthony.akentiev@gmail.com>
2023-10-23 12:19:24 +0100

    ERC20NameToken: custom code -> OZ implementation

--- contracts/anytype/ERC20NameToken.sol
+++ contracts/anytype/ERC20NameToken.sol
@@ -15,92 +15,26 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  *   Anytype Admin mints 1 token to a user
  *   User can use this token to buy/renew a name for 1 year
  */
-contract ERC20NameToken is Ownable, IERC20 {
-    string public name = "AnyNameToken";
-    string public symbol = "ANT";
-    uint8 public decimals = 6;
-    uint256 public total;
+contract ERC20NameToken is Ownable, ERC20 {
+    constructor() ERC20("AnyNameToken", "ANT") {}
 
-    mapping(address => uint256) balances;
-    mapping(address => mapping(address => uint256)) allowances;
-
-    constructor() {}
-
-    // use it for tests
-    function mint(address to, uint256 amount) external onlyOwner {
-        total += amount;
-        balances[to] += amount;
-
-        emit Transfer(address(0), to, amount);
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
     }
 
-    function burn(address account, uint amount) public onlyOwner {
-        uint256 accountBalance = balances[account];
-        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+    function mint(address user_, uint amount_) public onlyOwner {
+        _mint(user_, amount_);
+    }
 
-        unchecked {
-            balances[account] = accountBalance - amount;
-
-            // Overflow not possible: amount <= accountBalance <= totalSupply.
-            total -= amount;
-        }
-
-        emit Transfer(account, address(0), amount);
+    function burn(address user_, uint amount_) public onlyOwner {
+        _burn(user_, amount_);
     }
 
     function approveFor(
         address from,
         address spender,
         uint256 value
-    ) public onlyOwner returns (bool) {
-        allowances[from][spender] = value;
-        emit Approval(from, spender, value);
-        return true;
-    }
-
-    function totalSupply() external view returns (uint256) {
-        return total;
-    }
-
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
-    }
-
-    function transfer(address to, uint256 value) public returns (bool) {
-        require(balances[msg.sender] >= value, "Insufficient balance");
-        balances[msg.sender] -= value;
-        balances[to] += value;
-        emit Transfer(msg.sender, to, value);
-        return true;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) public returns (bool) {
-        require(balances[from] >= value, "Insufficient balance");
-        require(
-            allowances[from][msg.sender] >= value,
-            "Insufficient allowance"
-        );
-        balances[from] -= value;
-        balances[to] += value;
-        allowances[from][msg.sender] -= value;
-        emit Transfer(from, to, value);
-        return true;
-    }
-
-    function allowance(
-        address owner,
-        address spender
-    ) external view returns (uint256) {
-        return allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 value) public returns (bool) {
-        allowances[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
-        return true;
+    ) public onlyOwner {
+        _approve(from, spender, value);
     }
 }
